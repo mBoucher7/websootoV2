90352a14b85d28e45fc561a35098abe5f6a191aa
mBoucher7 <124088772+rhit-bouchemj@users.noreply.github.com>
2024-08-18 15:55:05 -0400

    recreate archive

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
+function extractExcerpt(post) {
+  if(!post.templateContent) return '';
+  if(post.templateContent.indexOf('</p>') > 0) {
+    let end = post.templateContent.indexOf('</p>');
+    return post.templateContent.substr(0, end+4);
+  }
+  return post.templateContent;
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
+  
   return {
     dir: {
       input: "src",
